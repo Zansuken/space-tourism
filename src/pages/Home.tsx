f651ae18cfd181ac0153680014d6aac82f7e6668
Zansuken <doom.sebastien.pro@gmail.com>
2022-02-17 19:37:54 +0100

    feat(nav+home): added responsiveness for desktop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,14 +17,17 @@ const Home: FC = () => {
                 <Logo />
                 <NavMenu />
             </header>
-            <section>
-                <h2>SO, YOU WANT TO TRAVEL TO</h2>
-                <h1>SPACE</h1>
-                <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+            <section className='home'>
+                <div className="home-intro">
+
+                    <h2>SO, YOU WANT TO TRAVEL TO</h2>
+                    <h1>SPACE</h1>
+                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                </div>
+                <div className='explore-wrapper'>
+                    <NavLink to='/destinations' className='explore-button'>EXPLORE</NavLink>
+                </div>
             </section>
-            <div className='explore-wrapper'>
-                <NavLink to='/destinations' className='explore-button'>EXPLORE</NavLink>
-            </div>
         </>
     )
 }
